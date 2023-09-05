c275ca11857532f4d26378c217bbeab8f5d87328
Sahil Chowdhury <sahils@wireless-10-145-6-51.public.utexas.edu>
2023-09-04 23:53:12 -0500

    added resume

--- websitecard.py
+++ websitecard.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "static/files"
 
 @app.route('/')
 def mainpage():
@@ -12,6 +13,10 @@ def contactwebpage():
 @app.route('/hobbies.html')
 def hobbies():
     return render_template('hobbies.html')
+
+@app.route('/download')
+def download():
+    return send_from_directory(app.config['UPLOAD_FOLDER'],"Resume.pdf", as_attachment=True)
 
 if __name__=='__main__':
     app.run(debug=True)

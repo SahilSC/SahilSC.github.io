57b1c2ba82ec943122dd93db826ee57943002024
Sahil Chowdhury <sahils@wireless-10-148-196-105.public.utexas.edu>
2023-09-08 02:24:46 -0500

    resume.pdf added

--- websitecard.py
+++ websitecard.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 app.config['SECRET_KEY'] = "213123"
 app.config['UPLOAD_FOLDER'] = "static/files"
 
